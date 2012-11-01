5e2beee9c011492cf7c2252d1c80da6c6d2db31a
Abhineet Majrikar <abhineet06@gmail.com>
2012-11-01 11:03:29 -0400

    Changes

--- DBMS/js/Categories.js
+++ DBMS/js/Categories.js
@@ -2,9 +2,11 @@ $("document").ready(
 	function()
 	{
 		
-		for(var i=0;i<50;i++)
+		for(var i=0;i<20;i++)
  		$("#ref").before('<tr class="tableRow" id="ref"><td>Category'+ (i+1) +'</td></tr>');
 	  
+	
+		
 		
 	}
 	);
