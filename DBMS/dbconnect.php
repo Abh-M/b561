b7ef6c5b078c86200f7298461f4b09b42d533b7a
Viknes <Viknes@192.168.2.4>
2012-11-06 20:40:13 -0500

    Adding a common DB connector

---
+++ DBMS/dbconnect.php
@@ -0,0 +1,8 @@
+<?php 
+$dbHost = "localhost"; 
+$dbUserName = "root"; 
+$dbPass = ""; 
+$dbName = "b561f12_27"; 
+$dbConnection = mysql_connect ($dbHost, $dbUserName, $dbPass) or die ("Cannot connect to host $dbHost with user $dbUserName and the password provided."); 
+mysql_select_db ($dbName) or die ("Database $dbName not found on host $dbHost");
+?>
